0e5b3277f7d9a0020c80a6bd0491b50082993327
Syahrizal As <56682176+alisadikinsyahrizal@users.noreply.github.com>
2020-04-30 10:43:31 +0700

    wq

--- app/src/main/java/com/izrael/nakulaedu/rest/ApiInterface.java
+++ app/src/main/java/com/izrael/nakulaedu/rest/ApiInterface.java
@@ -31,7 +31,7 @@ public interface ApiInterface {
     @Multipart
     @POST("upload.php")
     Call<RequestBody> uploadGambar(@Part MultipartBody.Part fileToUpload,
-                                   @Part ("submit") RequestBody userId);
+                                   @Part ("submit") String userId);
 
     @FormUrlEncoded
     @POST("api.php")
